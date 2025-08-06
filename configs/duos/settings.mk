86e7011bf44b9a43e9e20c95cc16fff8e7d01c51
Tommaso Polonelli <topolonelli@ethz.ch>
2025-08-06 14:08:13 +0200

    IMA appraisal test - untrusted certificate

--- configs/duos/settings.mk
+++ configs/duos/settings.mk
@@ -7,7 +7,7 @@ ARCH=riscv
 DDR_CFG=ddr3_1866_x16
 PARTITION_FILE=partition_$(STORAGE_TYPE).xml
 
-PACKAGES += "wireless-regdb wpasupplicant cvi-pinmux-cv181x bluez git zip unzip curl musl-tools keyutils"
+PACKAGES += "wireless-regdb wpasupplicant cvi-pinmux-cv181x bluez git zip unzip curl musl-tools keyutils ima-evm-utils"
 
 IMAGE_ADDITIONS += "aic8800-firmware"
 IMAGE_ADDITIONS += "ethernet-leds"
